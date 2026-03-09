56d44e620452671e2e2ad3ab558a8374300cb880
HandMatt <74497505+HandMatt@users.noreply.github.com>
2026-03-09 14:26:05 +0000

    Tidy base path handling and make build script GitHub Pages–ready

--- build.sh
+++ build.sh
@@ -1,6 +1,1 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-python3 src/main.py "/static-site-generator/"
-
+python3 src/main.py "/REPO_NAME/"
--- src/main.py
+++ src/main.py
@@ -10,19 +10,19 @@ dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 
 def main():
+    basepath = default_basepath
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
-    else:
-        basepath = "/"
 
-    print("Deleting docs directory...")
+    print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
-    print("Copying static files to docs directory...")
+    print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
